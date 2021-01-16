d41fa9aa70c8b4ca9dc98b0c87a1d8f356467233
= <=>
2021-01-16 23:42:15 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string adi = "hilal";
+            int yas = 20;
+
+
+        }
+    }
+}
