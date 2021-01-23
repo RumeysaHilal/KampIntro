5871299ea509e25244b00f97db200ec2cffa45ae
= <=>
2021-01-23 23:52:09 +0300

    23 Ocak sonu

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class MusteriManager
+    {
+        //İş sınıflarıdır //Business Classes
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
